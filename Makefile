486cae86a41dbcdc806e5b73af2b7b558b892978
jason-designer <1270653169@qq.com>
2022-03-24 22:59:09 +0800

    NJU-ProjectN/am-kernels ics2021 initialized
    NJU-ProjectN/am-kernels adc316af6e482e6444a9bd68bafc3a57e2cafdbc cpu-tests,Makefile: support target `gdb`

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-STUID = ysyx_22040000
-STUNAME = 张三
+STUID = ysyx_22040596
+STUNAME = ZengGuangSen
 
 # DO NOT modify the following code!!!
 
--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -3,7 +3,18 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  bool res = true;
+	if(ref_r->pc != pc) {
+		printf("pc different after executing inst at pc = %lx , right = %lx  wrong = %lx  \n", pc, ref_r->pc, pc);
+		res=false;
+	}
+	for(int i=0;i<32;i++){
+		if(ref_r->gpr[i] != cpu.gpr[i]){
+			printf("gpr[%2d] different after executing inst at pc = %lx , right = %lx  wrong = %lx  \n", i, pc, ref_r->gpr[i], cpu.gpr[i]);
+			res=false;
+		}
+	}
+	return res;
 }
 
 void isa_difftest_attach() {
