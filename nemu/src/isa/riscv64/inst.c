61cd3b4b5b1039ef78a341b03b2d1de083341028
jason-designer <1270653169@qq.com>
2022-03-24 22:38:58 +0800

    NJU-ProjectN/abstract-machine ysyx2204 initialized
    NJU-ProjectN/abstract-machine 112799e02a28d22f03cfe496fe36408112047a71 native,platform: fix compile error on ubuntu 21.10

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -22,6 +22,9 @@ enum {
 static word_t immI(uint32_t i) { return SEXT(BITS(i, 31, 20), 12); }
 static word_t immU(uint32_t i) { return SEXT(BITS(i, 31, 12), 20) << 12; }
 static word_t immS(uint32_t i) { return (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); }
+static word_t immR(uint32_t i) { return 0; }
+static word_t immB(uint32_t i) { return SEXT(BITS(i, 31, 31), 1) << 12 | (BITS(i, 30, 25) << 5) | (BITS(i, 11, 8) << 1) | (BITS(i, 7, 7) << 11); }
+static word_t immJ(uint32_t i) { return SEXT(BITS(i, 31, 31), 1) << 20 | (BITS(i, 30, 21) << 1) | (BITS(i, 20, 20) << 11) | (BITS(i, 19, 12) << 12); }
 
 static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, int type) {
   uint32_t i = s->isa.inst.val;
@@ -33,6 +36,9 @@ static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, 
     case TYPE_I: src1R(rs1);     src2I(immI(i)); break;
     case TYPE_U: src1I(immU(i)); break;
     case TYPE_S: destI(immS(i)); src1R(rs1); src2R(rs2); break;
+    case TYPE_R: src1R(rs1); src2R(rs2); break;
+    case TYPE_B: destI(immB(i)); src1R(rs1); src2R(rs2); break;
+    case TYPE_J: src2R(immJ(i)); break;
   }
 }
 
@@ -47,10 +53,22 @@ static int decode_exec(Decode *s) {
 }
 
   INSTPAT_START();
+  //arithmetic
   INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(dest) = src1 + s->pc);
+  INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi   , I, R(dest) = src1 + src2);
+  
+
+  //jump
+  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J, R(dest) = 4 + s->pc; s->dnpc = s->pc + src2);
+  INSTPAT("??????? ????? ????? ??? ????? 11001 11", jalr   , I, word_t t = s->pc + 4; s->dnpc = (src1 + src2) & (~(word_t)1); R(dest) = t);
+
+  //branch
+
+  //save and load
   INSTPAT("??????? ????? ????? 011 ????? 00000 11", ld     , I, R(dest) = Mr(src1 + src2, 8));
   INSTPAT("??????? ????? ????? 011 ????? 01000 11", sd     , S, Mw(src1 + dest, 8, src2));
 
+  //other
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
   INSTPAT_END();
